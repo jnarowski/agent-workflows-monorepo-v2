3b395145999d3234c35dc70a5c9dd42df8b5bd65
JP Narowski <jnarowski@gmail.com>
2025-10-29 13:38:48 -0600

    refactored sdk

--- apps/web/src/client/components/GitOperationsModal.tsx
+++ apps/web/src/client/components/GitOperationsModal.tsx
@@ -227,7 +227,7 @@ export function GitOperationsModal({
   return (
     <>
       <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent className="max-w-md">
+        <DialogContent className="sm:max-w-md">
           <DialogHeader>
             <DialogTitle>Git Operations</DialogTitle>
             <DialogDescription>
